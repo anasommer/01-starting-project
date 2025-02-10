bd0cf80b782e236417c4ad97e8cc1cc673414b5d
Ana Sommer <ana.sommer@omda.com>
2025-02-10 15:34:48 +0100

    bug fix

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,19 +4,20 @@ import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
+  imports: [TaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
-  @Input({ required: true }) name?: string;
+  @Input({ required: true }) name!: string;
   tasks = [
     {
       id: 't1',
       userId: 'u1',
       title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular.',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
       dueDate: '2025-12-31',
     },
     {
@@ -31,14 +32,12 @@ export class TasksComponent {
       userId: 'u3',
       title: 'Prepare issue template',
       summary:
-        'Prepare and describe an issue template which will help with project management.',
+        'Prepare and describe an issue template which will help with project management',
       dueDate: '2024-06-15',
     },
   ];
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => {
-      task.userId === this.userId;
-    });
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
 }
