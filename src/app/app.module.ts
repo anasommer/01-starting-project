2589ffc0776c7759d81e6d26fd9302a4f2f6d151
Ana Sommer <ana.sommer@omda.com>
2025-02-14 15:19:39 +0100

    done modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
+import { TasksModule } from './tasks/tasks.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { FormsModule } from '@angular/forms';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    CardComponent,
-    NewTaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
